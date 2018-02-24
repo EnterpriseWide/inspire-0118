d4c5b1100c1f76cc1cab0e84853df5db89cee21f
Mitch <liquidvisual@gmail.com>
2018-02-24 19:26:00 +1100

    Updates
    * Expanded hero functionality - includes video option
    * Hero only triggers when multiple slides are present
    * Cards improved for tablets
    * Latest blog cards added

--- src/assets/scripts/carousel.js
+++ src/assets/scripts/carousel.js
@@ -13,43 +13,70 @@ function launchCarousel() {
         //==================================================
 
         var $this = $(this);
-        var $carousel  = $('.lv-hero', $this);
-        var $prevArrow = $('.lv-hero-carousel-prev-btn', $this);
-        var $nextArrow = $('.lv-hero-carousel-next-btn', $this);
+        var $carousel   = $('.lv-hero', $this);
+        var $captions   = $('.lv-hero-item-caption', $this);
+        var $slideCount = $('.lv-hero-item', $carousel).length;
+        var $prevArrow  = $('.lv-hero-carousel-prev-btn', $this);
+        var $nextArrow  = $('.lv-hero-carousel-next-btn', $this);
 
         //==================================================
-        // FLICKITY
+        // CHECK FOR SLIDES
         //==================================================
 
-        var $flickity = $carousel.flickity({
-            adaptiveHeight: true,
-            autoPlay: false,
-            cellAlign: 'left',
-            contain: true,
-            dragThreshold: 3,
-            freeScroll: false,
-            freeScrollFriction: 0.075, // lower friction, slides easier
-            friction: 0.28, // Higher friction makes the slider feel stickier and less bouncy
-            imagesLoaded: true,
-            pageDots: true,
-            prevNextButtons: false,
-            pauseAutoPlayOnHover: false,
-            selectedAttraction: 0.025, // Higher attraction makes the slider move faster
-            // watchCSS: true, // run/destroy
-            wrapAround: true, // infinite
-        });
+        if ($slideCount > 1) {
 
-        //==================================================
-        // ARROWS
-        //==================================================
+            //==================================================
+            // FLICKITY
+            //==================================================
 
-        $prevArrow.on('click', function() {
-            $carousel.flickity('previous');
-        });
+            var $flickity = $carousel.flickity({
+                adaptiveHeight: true,
+                autoPlay: false,
+                cellAlign: 'left',
+                contain: true,
+                dragThreshold: 3,
+                freeScroll: false,
+                freeScrollFriction: 0.075, // lower friction, slides easier
+                friction: 0.28, // Higher friction makes the slider feel stickier and less bouncy
+                imagesLoaded: true,
+                pageDots: true,
+                prevNextButtons: false,
+                pauseAutoPlayOnHover: false,
+                selectedAttraction: 0.025, // Higher attraction makes the slider move faster
+                //watchCSS: true, // run/destroy
+                wrapAround: true, // infinite
+            });
 
-        $nextArrow.on('click', function() {
-            $carousel.flickity('next');
-        });
+            var flkty = $carousel.data('flickity');
+
+            //==================================================
+            // ARROWS
+            //==================================================
+
+            $prevArrow.on('click', function() {
+                $carousel.flickity('previous');
+            });
+
+            $nextArrow.on('click', function() {
+                $carousel.flickity('next');
+            });
+
+            //==================================================
+            // RETRIGGER CSS TRANSITIONS
+            //==================================================
+
+            $flickity.on('select.flickity', function() {
+                $captions.hide();
+
+                setTimeout(function(){
+                    $captions.show();
+                }, 500);
+            })
+
+            // $flickity.on('settle.flickity', function() {
+            //     $captions.show();
+            // });
+        }
     //--
     });
 }
