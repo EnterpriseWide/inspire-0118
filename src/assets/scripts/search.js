a1188a2c5628c3b520cf8c7974186941ff51a08c
liquidvisual <liquidvisual@gmail.com>
2018-07-09 13:06:39 +1000

    Global search feature - improved

--- src/assets/scripts/search.js
+++ src/assets/scripts/search.js
@@ -1,5 +1,5 @@
 /*
-    SEARCH.JS - Last updated: 03.07.17
+    SEARCH.JS - Last updated: 09.07.17
 */
 //-----------------------------------------------------------------
 // VARIABLES
@@ -8,31 +8,46 @@
 // SEARCH TRIGGER
 //-----------------------------------------------------------------
 
-$('[data-search-trigger]').click(function(event){
-
-    var $search = $('[data-search]');
-    //var searchIsHidden = $search.hasClass('is-hidden');
-    $search.removeClass('is-hidden');
-
-    $(document).on('scroll', function(){
-        $search.addClass('is-hidden');
-        // automatically close the keyboard on iOS
-        document.activeElement.blur();
-    });
-
+$('[data-search-trigger]').click(function(event) {
+    $('[data-global-search]').addClass('active');
+    $('[data-global-search] input').focus();
     return false;
 });
 
 //==============================================
-// LAUNCH MANAGE ON KEYPRESS
+// CLICK CLOSE
 //==============================================
 
-$('[data-search] .close').on('click', function(){
-    $('[data-search]').addClass('is-hidden');
-    // automatically close the keyboard on iOS
-    document.activeElement.blur();
+$('[data-global-search]').on('mousedown', function() {
+    closeSearch();
 });
+
+$('[data-global-search] input').on('mousedown', function(e) {
+    e.stopPropagation();
+})
+
+//==============================================
+// CLOSE ON ESC
+//==============================================
+
+$(document).keyup(function(e) {
+    if (e.keyCode === 27) {
+        closeSearch();
+    }
+});
+
+//==============================================
+// CLOSE
+//==============================================
+
+function closeSearch() {
+    $('[data-global-search]').removeClass('active');
+    document.activeElement.blur(); // automatically close the keyboard on iOS
+}
 
 //==================================================
 //
 //==================================================
+//-----------------------------------------------------------------
+//
+//-----------------------------------------------------------------
